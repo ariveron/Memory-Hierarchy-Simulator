b0f473b4576edcc62d1485c786d778cc7d56bc70
ariveron <45342341+ariveron@users.noreply.github.com>
2020-12-06 18:05:32 -0500

    Implement trace logger (#12)
    * implement logger header
    
    * implement logger footer
    
    * complete TraceLogger

--- src/TraceLogger.cpp
+++ src/TraceLogger.cpp
@@ -2,6 +2,7 @@
 #include "TraceConfig.h"
 
 #include <iostream>
+#include <iomanip>
 
 TraceLogger::TraceLogger(const TraceConfig& config)
   : Config { config }
@@ -9,21 +10,37 @@ TraceLogger::TraceLogger(const TraceConfig& config)
 
 void TraceLogger::PrintLog(int virtualAddress, int physicalAddress, bool isTLBHit, bool isPTHit, bool isDCHit) const
 {
-  // TODO
-  std::cout 
-    << "VA=" << virtualAddress << " PA=" << physicalAddress
-    << " TLBHit=" << isTLBHit << " PTHit=" << isPTHit << " DCHit=" << isDCHit
+  auto virtualPageNumber = virtualAddress >> Config.BitsPageTableOffset;
+  auto pageOffset = virtualAddress & ((2 << (Config.BitsPageTableOffset - 1)) - 1);
+  auto physicalPageNumber = physicalAddress >> Config.BitsPageTableOffset;
+  auto dataCacheTag = (physicalAddress & ((2 << (Config.BitsDataCacheTag + Config.BitsDataCacheIndex + Config.BitsDataCacheOffset - 1)) - 1))
+    >> (Config.BitsDataCacheIndex + Config.BitsDataCacheOffset);
+  auto dataCacheIndex = (physicalAddress & ((2 << (Config.BitsDataCacheIndex + Config.BitsDataCacheOffset - 1)) - 1))
+    >> (Config.BitsDataCacheOffset);
+
+  std::cout
+    << std::setw(8) << std::hex << std::setfill('0') << virtualAddress << std::setw(1) << std::setfill(' ') << " "
+    << std::setw(6) << std::hex << virtualPageNumber << std::setw(1) << " "
+    << std::setw(4) << std::hex << pageOffset << std::setw(1) << " "
+    << std::setw(4) << (isTLBHit ? "hit" : "miss") << std::setw(1) << " "
+    << std::setw(4) << (isPTHit ? "hit" : "miss") << std::setw(1) << " "
+    << std::setw(4) << std::hex << physicalPageNumber << std::setw(1) << " "
+    << std::setw(6) << std::hex << dataCacheTag << std::setw(1) << " "
+    << std::setw(3) << std::hex << dataCacheIndex << std::setw(1) << " "
+    << std::setw(4) << (isDCHit ? "hit" : "miss") << std::setw(1) << " "
     << std::endl;
 }
 
 void TraceLogger::PrintLogHeader() const
 {
-  // TODO
-  std::cout << "Log Header" << std::endl;
+  std::cout <<
+    "Virtual  Virt.  Page TLB  PT   Phys        DC  DC  \n"
+    "Address  Page # Off. Res. Res. Pg # DC Tag Idx Res.\n"
+    "-------- ------ ---- ---- ---- ---- ------ --- ----\n"
+    << std::flush;
 }
 
 void TraceLogger::PrintLogFooter() const
 {
-  // TODO
   std::cout << std::endl;
 }
