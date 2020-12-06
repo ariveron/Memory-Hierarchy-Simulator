aa936ea563599c9bd27a838ca41fac653ea663ee
Alex Riveron <riveron91@gmail.com>
2020-12-06 18:56:13 -0500

    implement CPU main driver logic

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -20,16 +20,50 @@ void CPU::Run(Trace& trace)
 
   Logger.PrintLogHeader();
 
+  // keep going until trace has no more entries
   while (!trace.IsDone())
   {
+    // Get the next trace entry
     auto entry = trace.Next();
 
+    // Keep track of reads and writes
     if (entry.IsWrite) TotalWrites++;
     else TotalReads++;
 
-    
+    // Get the physical address and TLB/PT info
+    int physicalAddress;
+    bool isTLBHit;
+    bool isPTHit;
 
-    Logger.PrintLog(entry.Address, entry.Address, true, true, true); 
+    // Make query to TLB if using VAs and TLB
+    if (Config.UseVirtualAddreses && Config.UseTLB)
+    {
+      auto tlbReturn = TLB.GetPhysicalAddress(entry.Address);
+      physicalAddress = tlbReturn.PhysicalAddress;
+      isTLBHit = tlbReturn.TLBHit;
+      isPTHit = tlbReturn.PTHit;
+    }
+    // Make query to PT if using VAs but not TLB
+    else if (Config.UseVirtualAddreses)
+    {
+      auto ptReturn = PT.GetPhysicalAddress(entry.Address);
+      physicalAddress = ptReturn.PhysicalAddress;
+      isTLBHit = false;
+      isPTHit = ptReturn.PTHit;
+    }
+    // If not using VA then the entry address is the PA
+    else
+    {
+      physicalAddress = entry.Address;
+      isTLBHit = false;
+      isPTHit = false;
+    }
+    
+    // Get info from DC about memory access
+    bool isDCHit = DC.GetBlock(physicalAddress).DCHit;
+
+    // Log output of processing this trace entry
+    Logger.PrintLog(entry.Address, physicalAddress, isTLBHit, isPTHit, isDCHit); 
   }
 
   Logger.PrintLogFooter();
